b194dc6f25d007037dbfb124f7b393718e336415
amiweb-7 <132199829+tib-dev@users.noreply.github.com>
2026-01-20 21:29:51 +0300

    Remove contents of orchestration/__init__.py
    Cleared all code from orchestration/__init__.py, removing Dagster Definitions and related imports. This may be in preparation for refactoring or restructuring the orchestration module.

--- orchestration/__init__.py
+++
@@ -1,9 +0,0 @@
-from dagster import Definitions
-
-from orchestration.pipeline import medical_telegram_pipeline
-from orchestration.schedules import daily_medical_telegram_pipeline
-
-defs = Definitions(
-    jobs=[medical_telegram_pipeline],
-    schedules=[daily_medical_telegram_pipeline],
-)
